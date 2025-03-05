aeda94a04a988ee9446d56d08232cd3a8c205571
viewsdevelop <viewsdevelop@gmail.com>
2025-03-05 11:57:59 -0500

    Create a CodeGrade submission

--- src/components/JokeDisplay.jsx
+++ src/components/JokeDisplay.jsx
@@ -1,10 +1,7 @@
-// Step 1: Accept `joke` and `loading` as props
-
 const JokeDisplay = ({ joke, loading }) => {
   return (
     <div className="joke-container">
-      {/* Step 2: If `loading` is true, display "Loading..." */}
-      {/* Step 3: Otherwise, display the joke */}
+      {loading ? <p>Loading...</p> : <p>{joke}</p>}
     </div>
   )
 }
